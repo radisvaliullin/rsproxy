bb9b65a77fd268f17e7e935544c3dee1b4f547a0
Radis Valiullin <radisrower@gmail.com>
2024-03-24 18:44:28 -0700

    rsproxy: conn handler, bidirection forwarder

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use async_std::{
-    io::{Read, Write},
-    net::TcpListener,
+    future,
+    io::{Error, ErrorKind, Read, Write},
+    net::{TcpListener, TcpStream},
     prelude::*,
     task::spawn,
 };
 use futures::stream::StreamExt;
+use futures::try_join;
+use std::time::Duration;
 
 // rust proxy prototype
 // async version
@@ -14,31 +17,88 @@ async fn main() {
 
     // config hardcode
     let addr = "0.0.0.0:4040";
+    let upstream_addr = "0.0.0.0:4044";
 
     // init listener
     let ln = TcpListener::bind(addr).await.unwrap();
     ln.incoming()
         .for_each_concurrent(None, |tcp_stream| async move {
-            let tcp_stream = tcp_stream.unwrap();
-            spawn(handle_connection(tcp_stream));
+            let stream = tcp_stream.unwrap();
+            spawn(handle_connection(stream, upstream_addr));
         })
         .await;
 
     println!("server stop.")
 }
 
-async fn handle_connection(mut stream: impl Read + Write + Unpin) {
-    // request read
-    let mut buffer = [0; 1024];
-    let read_len = match stream.read(&mut buffer).await {
-        Ok(n) => n,
+async fn handle_connection(stream: TcpStream, upstream_addr: &str) {
+    println!("conn handler: handling.");
+
+    // dial upstream connection
+    let upstream_conn_fut = TcpStream::connect(upstream_addr);
+    let uconn_timeout = Duration::from_millis(15_000);
+    let upstream = match future::timeout(uconn_timeout, upstream_conn_fut).await {
+        Ok(uconn_res) => match uconn_res {
+            Ok(stream) => stream,
+            Err(err) => {
+                println!("conn handler: upstream conn error: {:?}", err);
+                return;
+            }
+        },
         Err(err) => {
-            println!("stream read error: {:?}", err);
+            println!("conn handler: upstream conn timeout error: {:?}", err);
             return;
         }
     };
-    let received = std::str::from_utf8(&buffer[0..read_len]).expect("valid utf8");
-    println!("stream read: {:?}", received);
+    // clone stream for bidirection forwarding
+    let stream2 = stream.clone();
+    let upstream2 = upstream.clone();
 
-    println!("conn handled.")
+    // forward streams
+    if let Err(err) = try_join!(
+        forwarder("stream to upstream", stream, upstream),
+        forwarder("upstream to stream", upstream2, stream2),
+    ) {
+        println!("conn handler: forwarder err: {}", err)
+    };
+
+    println!("conn handler: done.");
+}
+
+async fn forwarder(
+    name: &str,
+    mut rstream: impl Read + Unpin,
+    mut wstream: impl Write + Unpin,
+) -> Result<(), Error> {
+    loop {
+        // read
+        let mut buffer = [0; 1024];
+        let read_len = match rstream.read(&mut buffer).await {
+            Ok(0) => {
+                println!("forwarder: rstream read 0 bytes, other side close connection");
+                return Result::Err(Error::from(ErrorKind::UnexpectedEof));
+            }
+            Ok(n) => n,
+            Err(err) => {
+                println!("forwarder: rstream read error: {:?}", err);
+                return Result::Err(err);
+            }
+        };
+        _ = name;
+        // // print for test
+        // let received = std::str::from_utf8(&buffer[0..read_len]).expect("valid utf8");
+        // println!("{} read: {:?}", name, received);
+
+        // write
+        let write_len = match wstream.write(&buffer[0..read_len]).await {
+            Ok(n) => n,
+            Err(err) => {
+                println!("forwarder: wstream write error: {:?}", err);
+                return Result::Err(err);
+            }
+        };
+        _ = write_len;
+        // // print for test
+        // println!("{} write len {}", name, write_len);
+    }
 }
